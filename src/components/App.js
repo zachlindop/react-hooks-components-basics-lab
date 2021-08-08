5d5fafd47ab3a96e714651f01224a5e665b2fd3b
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-08-07 20:42:54 -0700

    completed assignment!

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
